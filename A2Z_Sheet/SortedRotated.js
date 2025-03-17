c9132b2b5854bd32f32e6bbbdc3164572a77a9db
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-03-17 23:26:56 +0530

    update optimal solution for sorted and rotated array

--- A2Z_Sheet/SortedRotated.js
+++ A2Z_Sheet/SortedRotated.js
@@ -1,34 +1,14 @@
 // Is the given array sorted and rotated
 
 var check = function (nums) {
-  if (nums.length < 2) {
-    return true;
-  }
-  let violation = 0,
-    index = -1;
+  let count = 0,
+    len = nums.length;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > nums[i + 1]) {
-      violation++;
-      index = i + 1;
+    if (nums[i] > nums[(i + 1) % len]) {
+      count++;
     }
   }
-  if (violation === 0) {
-    return true;
-  }
-  if (violation > 1) {
-    return false;
-  }
-  if (violation === 1) {
-    const left = nums.slice(0, index);
-    const right = nums.slice(index);
-    const finalArr = [...right, ...left];
-    for (let i = 0; i < finalArr.length - 1; i++) {
-      if (finalArr[i] > finalArr[i + 1]) {
-        return false;
-      }
-    }
-  }
-  return true;
+  return count <= 1;
 };
 
 const nums = [3, 4, 5, 1, 2];
