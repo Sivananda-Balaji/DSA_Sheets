e9e3d8f8866104b2ef8f35664c95f11ac36dc141
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-03-22 01:55:26 +0530

    update sort color solution

--- A2Z_Sheet/sortArray.js
+++ A2Z_Sheet/sortArray.js
@@ -1,16 +1,25 @@
 //Sort an array
 
 var sortColors = function (nums) {
+  let zero = 0,
+    one = 0,
+    two = 0;
   for (let i = 0; i < nums.length; i++) {
-    let isSwap = false;
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[j] > nums[j + 1]) {
-        isSwap = true;
-        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
-      }
+    if (nums[i] === 0) {
+      zero++;
+    } else if (nums[i] === 1) {
+      one++;
+    } else {
+      two++;
     }
-    if (!isSwap) {
-      return nums;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (i < zero) {
+      nums[i] = 0;
+    } else if (i < zero + one) {
+      nums[i] = 1;
+    } else {
+      nums[i] = 2;
     }
   }
   return nums;
