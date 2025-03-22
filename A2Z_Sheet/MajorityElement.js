956b8662f2951100da81e11f380242b268ad407f
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-03-22 15:00:13 +0530

    update majority element solution

--- A2Z_Sheet/MajorityElement.js
+++ A2Z_Sheet/MajorityElement.js
@@ -1,16 +1,25 @@
 //169. Majority Element
 
 var majorityElement = function (nums) {
-  const limit = Math.floor(nums.length / 2);
-  const map = {};
+  let ele = -1,
+    count = 0;
   for (let i = 0; i < nums.length; i++) {
-    map[nums[i]] ? map[nums[i]]++ : (map[nums[i]] = 1);
-  }
-  for (let i in map) {
-    if (map[i] > limit) {
-      return Number(i);
+    if (count === 0) {
+      ele = nums[i];
+      count = 1;
+    } else if (ele !== nums[i]) {
+      count--;
+    } else {
+      count++;
     }
   }
+  count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (ele === nums[i]) {
+      count++;
+    }
+  }
+  return count > Math.floor(nums.length / 2) ? ele : -1;
 };
 
 const nums = [3, 2, 3];
