cc2f2f8723087dcccc84ab4fa9767b357da5b82d
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-03-30 09:04:42 +0530

    update rotate image solution

--- A2Z_Sheet/RotateImage.js
+++ A2Z_Sheet/RotateImage.js
@@ -1,19 +1,15 @@
 // 48. Rotate Image
 
 var rotate = function (matrix) {
-  const ans = Array.from({ length: matrix.length }, () =>
-    Array(matrix[0].length).fill(0)
-  );
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      ans[j][matrix.length - 1 - i] = matrix[i][j];
+    for (let j = i + 1; j < matrix[i].length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
     }
   }
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      matrix[i][j] = ans[i][j];
-    }
+    matrix[i] = matrix[i].reverse();
   }
+  return matrix;
 };
 
 const matrix = [
