f14dbe6a16656eb0b9a65fb070534b7051ded84b
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-04-06 23:28:16 +0530

    update pascal triangle 2 solution

--- Arrays_Medium/PascalTriangle_II.js
+++ Arrays_Medium/PascalTriangle_II.js
@@ -1,17 +1,14 @@
 //Pascal's Triangle II
 
 function pascalTriangleII(r) {
-  let ans = [];
-  for (let i = 1; i <= r; i++) {
-    ans.push(new Array(i).fill(1));
+  let ans = 1;
+  const res = [1];
+  for (let i = 1; i < r; i++) {
+    ans *= r - i;
+    ans /= i;
+    res.push(ans);
   }
-  for (let i = 1; i < ans.length - 1; i++) {
-    for (let j = 0; j < ans[i].length - 1; j++) {
-      const val = ans[i][j] + ans[i][j + 1];
-      ans[i + 1][j + 1] = val;
-    }
-  }
-  return ans[r - 1];
+  return res;
 }
 
 const r = 4;
