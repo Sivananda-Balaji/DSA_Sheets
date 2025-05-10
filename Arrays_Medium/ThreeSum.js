5ab4dac462de823a1f7593cc5a89e445a8e98b4b
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-05-10 12:50:18 +0530

    update three sum optimal solution

--- Arrays_Medium/ThreeSum.js
+++ Arrays_Medium/ThreeSum.js
@@ -1,19 +1,34 @@
 //3 Sum
 
 function threeSum(nums) {
-  const ans = new Set();
+  nums.sort((a, b) => a - b);
+  const ans = [];
   for (let i = 0; i < nums.length - 1; i++) {
-    const obj = {};
-    for (let j = i + 1; j < nums.length; j++) {
-      const val = -(nums[i] + nums[j]);
-      if (obj[val] !== undefined) {
-        const temp = [nums[i], nums[j], obj[val]].sort((a, b) => a - b);
-        ans.add(temp.toString());
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let j = i + 1,
+      k = nums.length - 1;
+    while (j < k) {
+      const sum = nums[i] + nums[j] + nums[k];
+      if (sum === 0) {
+        ans.push([nums[i], nums[j], nums[k]]);
+        k--;
+        while (nums[k] === nums[k + 1]) {
+          k--;
+        }
+        j++;
+        while (nums[j] === nums[j - 1]) {
+          j++;
+        }
+      } else if (sum > 0) {
+        k--;
+      } else {
+        j++;
       }
-      obj[nums[j]] = nums[j];
     }
   }
-  return Array.from(ans).map((str) => str.split(",").map(Number));
+  return ans;
 }
 
 const nums = [2, -2, 0, 3, -3, 5];
