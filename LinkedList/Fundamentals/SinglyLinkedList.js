7dabc11250e984b20b9d0d9cc90619369753d2ad
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-06-08 13:21:51 +0530

    add append and insert method to singly linked list

--- LinkedList/Fundamentals/SinglyLinkedList.js
+++ LinkedList/Fundamentals/SinglyLinkedList.js
@@ -26,6 +26,37 @@ class LinkedList {
     this.head = node;
     this.size++;
   }
+  append(data) {
+    const node = new Node(data);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let curr = this.head;
+      while (curr.next !== null) {
+        curr = curr.next;
+      }
+      curr.next = node;
+    }
+    this.size++;
+  }
+  insert(data, index) {
+    if (index < 0 || index > this.getSize()) {
+      console.log("Not a Valid Index");
+      return;
+    }
+    if (index === 0) {
+      this.prepand(data);
+    } else if (index > 0) {
+      const node = new Node(data);
+      let curr = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        curr = curr.next;
+      }
+      node.next = curr.next;
+      curr.next = node;
+      this.size++;
+    }
+  }
   print() {
     if (this.isEmpty()) {
       console.log("list is empty");
@@ -47,5 +78,8 @@ console.log("list size", list.getSize());
 list.prepand(10);
 list.prepand(20);
 list.prepand(30);
+list.append(100);
+
+list.insert(50, 2);
 
 list.print();
