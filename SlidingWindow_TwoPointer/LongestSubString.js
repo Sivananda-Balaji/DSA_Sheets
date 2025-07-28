3ce53262cf4ccaa48ffbf4a5c21d765e384bc197
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-07-28 06:51:04 +0530

    update longest substring solution

--- SlidingWindow_TwoPointer/LongestSubString.js
+++ SlidingWindow_TwoPointer/LongestSubString.js
@@ -6,17 +6,14 @@ function longestNonRepeatingSubstring(s) {
     max = 0,
     curr = 0;
   for (let i = 0; i < s.length; i++) {
-    if (!str.includes(s[i])) {
-      str += s[i];
-      curr++;
-    } else {
-      while (str.includes(s[i])) {
-        str = str.substring(1);
-        curr--;
-      }
-      str += s[i];
-      curr++;
+    if (str.includes(s[i])) {
+      const index = str.indexOf(s[i]);
+      const after = str.substring(index + 1);
+      str = after;
+      curr = after.length;
     }
+    str += s[i];
+    curr++;
     max = Math.max(curr, max);
   }
   return max;
