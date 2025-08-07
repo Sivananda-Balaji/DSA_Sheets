994d40cb35cbf2b1487d508b5022451cd16e2aba
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-08-07 14:27:40 +0530

    update merge sorted array solution without space

--- Arrays_Hard/MergeSortedArray.js
+++ Arrays_Hard/MergeSortedArray.js
@@ -1,36 +1,27 @@
 //Merge two sorted arrays without extra space
 
 function merge(nums1, m, nums2, n) {
-  const ans = [];
-  let p1 = 0,
-    p2 = 0;
-  while (p1 < m && p2 < n) {
-    if (nums1[p1] <= nums2[p2]) {
-      ans.push(nums1[p1]);
-      p1++;
+  let index = nums1.length - 1,
+    p1 = m - 1,
+    p2 = n - 1;
+  while (p2 >= 0) {
+    if (nums1[p1] >= nums2[p2]) {
+      nums1[index] = nums1[p1];
+      index--;
+      p1--;
     } else {
-      ans.push(nums2[p2]);
-      p2++;
+      nums1[index] = nums2[p2];
+      index--;
+      p2--;
     }
-  }
-  while (p1 < m) {
-    ans.push(nums1[p1]);
-    p1++;
-  }
-  while (p2 < n) {
-    ans.push(nums2[p2]);
-    p2++;
-  }
-  for (let i = 0; i < ans.length; i++) {
-    nums1[i] = ans[i];
   }
   return nums1;
 }
 
-const nums1 = [-5, -2, 4, 5],
+const nums1 = [-5, -2, 4, 5, 0, 0, 0],
   nums2 = [-3, 1, 8],
-  m = nums1.length,
-  n = nums2.length;
+  m = 4,
+  n = 3;
 
 const result = merge(nums1, m, nums2, n);
 
